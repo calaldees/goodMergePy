62820c4bd0fd366524070eab9df9c0657049f0f2
Allan Callaghan <calaldees@gmail.com>
2017-09-07 18:41:33 +0100

    Improved logging output Added output as json Added dryrun xmdb is optional

--- Makefile
+++ Makefile
@@ -1,12 +1,18 @@
-
 help:
 	#
+
+config.json:
+	cp config.dist.json $@
+
+install: config.json
 
 var/xmdb:
 	mkdir -p $*
 	curl https://downloads.sourceforge.net/project/goodmerge/GoodMerge%20XMDBs/XMDBs%20%28Goodtools%20v0.xx%20-%20v3.14%29.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fgoodmerge%2Ffiles%2F
 	unzip
 
+run:
+	python3 goodMerge.py --postmortem --log_level 0 --path_filelist ./var/rom_lists/gba.txt --type gba
+
 test:
 	pytest --pdb --doctest-modules
-
--- goodMerge.py
+++ goodMerge.py
@@ -111,8 +111,6 @@ def group_filelist(filelist, merge_data={}):
     '{"Rom Name 2 - More Example": ["Rom Name 2 - More Example Better [E].zip", "Rom Name 2 - More Example [U] [!].zip", "Rom Name J - Japanese name [J].zip"], "Unrelated Name": ["Unrelated Name.zip"]}'
 
     """
-    log.info('merge')
-    log.info(f'{len(filelist)} in filelist')
 
     def _normalize_filename(filename):
         normalized_filename = re.match(r'[^([]+', filename).group(0).strip()
@@ -159,15 +157,17 @@ def group_filelist(filelist, merge_data={}):
 class CompressionHelperTempfolder():
     """
     """
-    def __init__(self, source_folder=None, destination_folder=None, working_folder=None, cmd_compress="""7z a '{destination_file}'""", cmd_decompress="""7z e -o'{destination_folder}'""", cmd_call=subprocess.call, cmd_remove=os.remove, cmd_move=shutil.move, cmd_listdir=os.listdir, **kwargs):
+    def __init__(self, source_folder=None, destination_folder=None, working_folder=None, cmd_compress="""7z a {destination_file}""", cmd_decompress="""7z e -o{destination_folder}""", cmd_call=subprocess.call, cmd_remove=os.remove, cmd_move=shutil.move, cmd_listdir=os.listdir, compressed_extension='7z', **kwargs):
+        assert source_folder
         self.source_folder = os.path.abspath(source_folder)
         assert os.path.isdir(self.source_folder)
         self.destination_folder = os.path.abspath(destination_folder or source_folder)
         assert os.path.isdir(self.destination_folder)
         self.working_folder = os.path.abspath(working_folder) if working_folder else None
+        self.compressed_extension = compressed_extension
 
         def _cmd_call_string(cmd_string, *args, **kwargs):
-            return cmd_call(cmd_string.format(**kwargs).split(' ') + args)
+            return cmd_call(tuple(cmd_string.format(**kwargs).split(' ')) + args)
 
         self.cmd = {
             'compress': partial(_cmd_call_string, cmd_compress),
@@ -183,7 +183,7 @@ class CompressionHelperTempfolder():
         if not self.working_folder:
             self.temp_folder_object = TemporaryDirectory()
             self.working_folder = self.temp_folder_object.name
-        assert os.path.isdir(self.working_folder)
+        assert self.working_folder and os.path.isdir(self.working_folder)
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
@@ -192,25 +192,36 @@ class CompressionHelperTempfolder():
             self.working_folder = None
 
     def prepare(self, source_filename):
+        def current_working_files_count():
+            return len(self.cmd['listdir'](self.working_folder))
+        starting_working_file_count = current_working_files_count()
         source_filename = os.path.join(self.source_folder, source_filename)
+        assert os.path.exists(source_filename)
         if REGEX_IS_COMPRESSED_FILE_EXTENSION.search(source_filename):
             self.cmd['decompress'](source_filename, destination_folder=self.working_folder)
-            self.cmd['remove'](source_filename)
         else:
             self.cmd['move'](source_filename, self.working_folder)
+        assert starting_working_file_count < current_working_files_count()
+        if os.path.exists(source_filename):
+            self.cmd['remove'](source_filename)
+        assert not os.path.exists(source_filename)
 
     def compress(self, destination_filename):
-        destination_filename = os.path.join(self.destination_folder, destination_filename)
+        destination_filename = os.path.join(self.destination_folder, f'{destination_filename}.{self.compressed_extension}')
         working_filenames = self.cmd['listdir'](self.working_folder)
+        assert not os.path.isfile(destination_filename)
         self.cmd['compress'](*working_filenames, destination_file=destination_filename)
+        assert os.path.isfile(destination_filename)
         for filename in working_filenames:
             self.cmd['remove'](filename)
+            assert not os.path.exists(filename)
 
 
 def merge(grouped_filelist, compressor):
     for destination_filename, source_filenames in grouped_filelist.items():
         for source_filename in source_filenames:
             compressor.prepare(source_filename)
+        log.info(destination_filename)
         compressor.compress(destination_filename)
 
 
@@ -224,12 +235,14 @@ def get_args():
         description=DESCRIPTION,
     )
 
-    parser.add_argument('type', action='store', help='')
+    parser.add_argument('--type', action='store', help='')
     parser.add_argument('--path_xmdb', action='store', help='')
     parser.add_argument('--path_roms', action='store', help='')
     parser.add_argument('--path_filelist', action='store', help='for debugging')
 
+
     parser.add_argument('--config', action='store', help='', default=DEFAULT_CONFIG_FILENAME)
+    parser.add_argument('--dryrun', action='store_true', help='Dont compress files and output json')
     parser.add_argument('--postmortem', action='store_true', help='Enter debugger on exception')
     parser.add_argument('--log_level', type=int, help='log level')
     parser.add_argument('--version', action='version', version=VERSION)
@@ -258,14 +271,28 @@ def get_args():
 # Main -------------------------------------------------------------------------
 
 def main(**kwargs):
-    grouped_filelist = group_filelist(
-        filelist=get_filelist(path_roms=kwargs.get('source_folder'), path_filelist=kwargs.get('path_filelist')),
-        merge_data=parse_xmdb_dom(_load_xml(
-            os.path.join(kwargs['path_xmdb'], TEMPLATE_FILENAME_XMDB.format(kwargs['type']))
-        )),
-    )
-    with CompressionHelperTempfolder(**kwargs) as compressor:
-        merge(grouped_filelist, compressor)
+    filelist = get_filelist(path_roms=kwargs.get('source_folder'), path_filelist=kwargs.get('path_filelist'))
+    assert filelist
+
+    # Load group data
+    xmdb_data = {}
+    if kwargs.get('path_xmdb') and kwargs.get('type'):
+        xmdb_filename = os.path.join(kwargs['path_xmdb'], TEMPLATE_FILENAME_XMDB.format(kwargs['type']))
+        assert os.path.isfile(xmdb_filename)
+        log.info(f'Loading xmdb: {xmdb_filename}')
+        xmdb_data = parse_xmdb_dom(_load_xml(xmdb_filename))
+
+    # Grouping Logic
+    grouped_filelist = group_filelist(filelist=filelist, merge_data=xmdb_data)
+    log.info(f'filelist: {len(filelist)} grouped_filelist: {len(grouped_filelist.keys())}')
+
+    if kwargs.get('source_folder') and not kwargs.get('dryrun'):
+        # Merge & Compress
+        with CompressionHelperTempfolder(**kwargs) as compressor:
+            merge(grouped_filelist, compressor)
+    else:
+        # Output json
+        print(json.dumps(grouped_filelist, cls=SetEncoder))
 
 
 def postmortem(func, *args, **kwargs):
